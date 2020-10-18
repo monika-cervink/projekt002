e37ddb84a8f56bd001b221314a08b4f670350bde
Monika Červinková <monika.cervink@gmail.com>
2020-10-18 17:46:44 +0200

    drobné opravy

--- bulls_cows.py
+++ bulls_cows.py
@@ -1,25 +1,30 @@
 import random
 import time
-generator = random.sample((range(0, 10)), 4)
-generator = [str(i) for i in generator]
+
+secret_nums = random.sample((range(0, 10)), 4)
+secret_nums = [str(i) for i in secret_nums]
 
 print("Hi there! Let's play Bulls & Cows game!")
 print("I've generated a random number for you (4 digit).")
+
 start = time.time()
 counter = 0
+
 while counter >= 0:
     guess = input("Enter a number: ")
-    while len(guess) != 4 or not guess.isdigit():
-        print("Enter a 4 digit number!")
-        guess = input("Enter a number: ")
     bulls = 0
     cows = 0
+    if len(guess) != 4 or not guess.isdigit():
+        print("Enter a 4 digit number!")
+        continue
+
     for seq, number in enumerate(guess):
-        for seq2, number2 in enumerate(generator):
+        for seq2, number2 in enumerate(secret_nums):
             if seq == seq2 and number == number2:
                 bulls += 1
             elif seq != seq2 and number == number2:
                 cows += 1
+
     if cows != 1 and bulls == 1:
         print(f"{bulls} bull, {cows} cows")
     elif cows == 1 and bulls != 1:
@@ -28,13 +33,14 @@ while counter >= 0:
         print(f"{bulls} bull, {cows} cow")
     else:
         print(f"{bulls} bulls, {cows} cows")
+
     counter += 1
     if bulls == 4:
         break
+
 end = time.time()
 timer = end - start
-minutes = round(timer // 60)
-seconds = round(timer % 60)
+elapsed_time = time.strftime("%H:%M:%S", time.gmtime(end - start))
 
 if counter <= 10:
     level = "amazing!"
@@ -43,7 +49,8 @@ elif 10 < counter <= 20:
 else:
     level = "not so good..."
 
-print(f"Great, you've guessed the right number in {counter} guesses! "
-      f"That's {level} \nYou did it in {minutes} min {seconds} sec.")
+print(f"Great, you've guessed the right number in {counter} guesses! That's {level}")
+print(f"Your time: {elapsed_time}.")
+
 with open("number_of_guesses.txt", "a+") as file:
     file.write(f"{counter}\n")
